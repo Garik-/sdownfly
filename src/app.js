f2e8f44aa5f6231dd58bcdff3f016ae5498b4019
Gar|k <garik.djan@gmail.com>
2025-12-09 22:03:08 +0300

    feat: add get caps

--- src/app.js
+++ src/app.js
@@ -4,6 +4,21 @@
 import { music } from './jackett.js'
 import { save } from './data.js'
 
+/*
+async function getCapabilities() {
+  const caps = await config('rutracker')
+
+  const regex = /lossless|hi\-res|audio/i
+  const keys = []
+  for (const key in caps) {
+    if (regex.test(caps[key])) {
+      keys.push(key)
+    }
+  }
+  return keys.join(',')
+}
+  */
+
 async function main() {
   try {
     // const spotify = await init()
@@ -14,10 +29,10 @@ async function main() {
     // )
     // console.log('Playlist data:', JSON.stringify(p, null, 2))
 
-    const results = await music({ artist: 'Trivium', album: 'Shogun',year:'2008', q: 'Trivium Shogun 2008' })
+    // const cat = await getCapabilities()
+    const results = await music({ artist: 'Trivium', album: '', year: '', q: 'Trivium' })
     await save('jackett_results.json', results)
     console.log('Jackett search completed. Results saved to jackett_results.json')
-
   } catch (err) {
     console.error('Error during initialization:', err)
     process.exit(1)
--- src/jackett.js
+++ src/jackett.js
@@ -11,18 +11,72 @@ if (!JACKETT_URL) {
   throw new Error('JACKETT_URL environment variable is not set.')
 }
 
-export async function music({ album, artist, label, track, year, genre, q } = {}) {
-  const url = new URL('/api/v2.0/indexers/all/results/torznab/api', JACKETT_URL)
+export async function config(indexers = '!status:failing,test:passed') {
+  const url = new URL(`/api/v2.0/indexers/${indexers}/results/torznab/api`, JACKETT_URL)
+  url.searchParams.append('apikey', JACKETT_API_KEY)
+  url.searchParams.append('t', 'caps')
+
+  try {
+    const res = await fetch(url)
+
+    if (!res.ok) {
+      throw new Error(`Jackett returned HTTP ${res.status}: ${res.statusText}`)
+    }
+
+    const xml = await res.text()
+
+    let data
+    try {
+      data = await xml2js.parseStringPromise(xml, {
+        explicitArray: false,
+      })
+    } catch (e) {
+      throw new Error('Failed to parse XML from Jackett: ' + e.message)
+    }
+
+    const map = {}
+    for (const item of data.caps.categories.category) {
+      const { id, name } = item['$']
+      map[id] = name
+
+      if (item.subcat) {
+        if (Array.isArray(item.subcat)) {
+          for (const sub of item.subcat) {
+            const { id: subId, name: subName } = sub['$']
+            map[subId] = subName
+          }
+        } else {
+          const { id: subId, name: subName } = item.subcat['$']
+          map[subId] = subName
+        }
+      }
+    }
+    return map
+  } catch (err) {
+    console.error('Jackett music search error:', err.message)
+    return null
+  }
+}
+
+export async function music({ album, artist, label, track, year, genre, q, cat } = {}) {
+  const url = new URL(
+    '/api/v2.0/indexers/!status:failing,test:passed/results/torznab/api',
+    JACKETT_URL,
+  )
   url.searchParams.append('apikey', JACKETT_API_KEY)
   url.searchParams.append('t', 'music')
 
-  const params = { album, artist, label, track, year, genre, q }
+  const params = { album, artist, label, track, year, genre, q, cat }
 
   for (const [key, value] of Object.entries(params)) {
     if (value !== undefined && value !== null && value !== '') {
       url.searchParams.append(key, value)
     }
   }
+
+  console.log(cat)
+
+  // console.log(url.toString())
 
   try {
     const res = await fetch(url)
